584c8573b3f123a30531afb56c2b30eb2b2ff142
Abhilash Khare <abhilashkhare@gmail.com>
2018-10-20 02:27:11 -0700

    feat : Add Label for last 4 digits and add TextDelgate

--- Pay O'Clock/ViewController.swift
+++ Pay O'Clock/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var lastFourDigits : UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.lastFourDigits.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
