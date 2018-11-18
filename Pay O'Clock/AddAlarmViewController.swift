dda0daa74232731caee5b219b583faf4607925b6
Abhilash Khare <abhilashkhare@gmail.com>
2018-11-17 18:04:10 -0800

    feat: Change name of VC to AddAlarmVC

--- Pay O'Clock/AddAlarmViewController.swift
+++ Pay O'Clock/AddAlarmViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class AddAlarmViewController: UIViewController,UITextFieldDelegate {
     
     @IBOutlet var lastFourDigits : UITextField!
 
